70569c91069224914eadf4ac8ecb0844ba4b8f00
Loyal Chandu <228r1a0524@cmrec.ac.in>
2025-11-09 15:00:44 +0530

    ready for deploy

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,5 @@
+import 'dotenv/config';
+
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
@@ -73,9 +75,9 @@ app.use((req, res, next) => {
   const port = parseInt(process.env.PORT || '5000', 10);
   server.listen({
     port,
-    host: "0.0.0.0",
-    reusePort: true,
+    host: "127.0.0.1",
   }, () => {
     log(`serving on port ${port}`);
   });
 })();
+
